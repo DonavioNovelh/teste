b598219ad171f7f022b808144d3ab33144950ab1
DonavioNovelh <donavioev@gmail.com>
2025-01-21 21:56:13 +0200

    nn

---
+++ src/AdicionarTarefa.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const AdicionarTarefa = () => {
+  return (
+    <div>
+      <h1>Create Task</h1>
+      <div className="form">
+        <div className="filho">
+          <label htmlFor="">task Name</label>
+          <input type="text" name="" id="" />
+        </div>
+
+        <div className="filho">
+          <label htmlFor="">End Date</label>
+          <input type="text" name="" id="" />
+        </div>
+
+        <div className="filho">
+          <label htmlFor="">Priority</label>
+          <input type="text" name="" id="" />
+        </div>
+
+        <div className="filho">
+          <label htmlFor="">Descriptio</label>
+          <textarea placeholder='Descrever' name="" id=""></textarea>
+        </div>
+      </div>
+      <button>Save</button>
+    </div>
+  );
+}
+
+export default AdicionarTarefa
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, {useMemo, useReducer, useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdicionarNovaTarefa from "./AdicionarNovaTarefa";
+import AdicionarTarefa from "./AdicionarTarefa";
 
 export default function App() {
   return (
     
    <>
-   <AdicionarNovaTarefa/>
+   {/*<AdicionarNovaTarefa/>*/}
+   <AdicionarTarefa/>
    </>
   );
 }
